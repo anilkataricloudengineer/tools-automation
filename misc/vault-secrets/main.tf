81974a75912588460e278a0fe1b5e2c688009feb
Anil Katari <anilkataricloudengineer@gmail.com>
2025-12-28 00:57:37 +0530

    Roboshop Project

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -25,6 +25,37 @@ variable "secrets" {
         MONGO = "true"
         MONGO_URL = "mongodb://mongodb-dev.anilcloudengineer.online:27017/catalogue"
       }
+      user = {
+        MONGO     = "true"
+        REDIS_URL = "redis://redis-dev.anilcloudengineer.online:6379"
+        MONGO_URL = "mongodb://mongodb-dev.anilcloudengineer.online:27017/users"
+      }
+      cart = {
+        REDIS_HOST = "redis-dev.anilcloudengineer.online"
+        CATALOGUE_HOST = "catalogue-dev.anilcloudengineer.online"
+        CATALOGUE_PORT = "8080"
+      }
+      shipping = {
+        CART_ENDPOINT = "cart-dev.anilcloudengineer.online:8080"
+        DB_HOST = "mysql-dev.anilcloudengineer.online"
+      }
+      payment = {
+       CART_HOST = "cart-dev.anilcloudengineer.online"
+       CART_PORT = "8080"
+        USER_HOST = "user-dev.anilcloudengineer.online"
+        USER_PORT = "8080"
+        AMQP_HOST = "rabbitmq-dev.anilcloudengineer.online"
+        AMQP_USER = "roboshop"
+        AMQP_PASS = "roboshop123"
+      }
+      mysql = {
+        MYSQL_ROOT_PASSWORD = "RoboShop@1"
+
+      }
+      rabbitmq = {
+        AMQP_USER = "roboshop"
+        AMQP_PASS = "roboshop123"
+      }
     }
   }
 }
