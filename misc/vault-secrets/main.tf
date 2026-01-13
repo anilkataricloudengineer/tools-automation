82e114fcd59516bd3fc847521f46fc7439e111cd
Anil Katari <anilkataricloudengineer@gmail.com>
2026-01-13 22:17:12 +0530

    Roboshop Project

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -52,6 +52,11 @@ variable "secrets" {
       shipping = {
         CART_ENDPOINT = "cart-dev.anilcloudengineer.online:8080"
         DB_HOST = "mysql-dev.anilcloudengineer.online"
+        DB_TYPE = "mysql"
+        APP_GIT_URL = "https://github.com/roboshop-devops-project-v3/shipping"
+        DB_HOST = "mysql-dev.anilcloudengineer.online"
+        DB_USER = "root"
+        DB_PASS = "RoboShop@1"
       }
       payment = {
        CART_HOST = "cart-dev.anilcloudengineer.online"
